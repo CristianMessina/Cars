6981002d82669ecfc7f82901084b84e8020ac0d4
Cristian Messina <messina1399@gmail.com>
2021-03-23 12:43:39 +0100

    Aggiungere i file di progetto.

---
+++ Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cars.Models
+{
+    public class Car
+    {
+         public int ID { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Colour { get; set; }
+        public string Price { get; set; }
+    }
+}
